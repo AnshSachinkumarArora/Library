745fc68ca7db4dea69790c0df983abf20ca7606c
LordStubb <ansh47arora47@gmail.com>
2023-08-29 16:11:59 -0400

    fixed js issue and addBook now works correctly

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const showDialog = document.getElementById("add");
 const dialog = document.getElementById("addBook"); 
 const books = document.getElementById("books");
-const name = document.getElementById("name");
+const title = document.getElementById("name");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const read = document.getElementById("read");
@@ -18,8 +18,18 @@ function Book(author, title, pages) {
 function addBook(author, title, pages) {
     const temp = new Book(author, title, pages);
     myLibrary.push(temp);
+    
 }
 
 showDialog.addEventListener("click", () => {
     dialog.showModal();
 });
+
+dialog.addEventListener("close", (e) => {
+    addBook(author.value, title.value, pages.value);
+})
+
+confirm.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog.close();
+})
